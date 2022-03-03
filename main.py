40bf95d370b2bcf4843df1ffd58e1ae1a2fc9a35
Arunanshu22 <84843013+Arunanshu22@users.noreply.github.com>
2022-03-03 09:35:12 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-import seaborn as sbs
-from sklearn import preprocessing
-#list of all data desc:
-df = pd.read_csv("heart.csv")
-#list = [df[0]]
-print(df)
